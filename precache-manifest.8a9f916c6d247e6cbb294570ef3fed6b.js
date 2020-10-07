1e1e0a10f4a798ae4f3674d41227d333e4e61a3f
trunov <k.trunov.k@yandex.ru>
2020-10-07 20:00:55 +0300

    Updates

---
+++ precache-manifest.8a9f916c6d247e6cbb294570ef3fed6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78634a9707cb3ea075815aff44a6031",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b71a9495d513909efa9e",
+    "url": "./static/css/main.91f6d13d.chunk.css"
+  },
+  {
+    "revision": "e1ea9895b4833c307dc6",
+    "url": "./static/js/2.5f1b834b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5f1b834b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b71a9495d513909efa9e",
+    "url": "./static/js/main.df9dcede.chunk.js"
+  },
+  {
+    "revision": "b793c38fc953fa14b65d",
+    "url": "./static/js/runtime-main.208b55f4.js"
+  }
+]);
